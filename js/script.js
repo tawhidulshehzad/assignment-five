aaeaeb003610448851b33cacb3d708fe0e921bf1
shehzad1212 <tawhidulislam261@gmail.com>
2025-03-02 14:44:48 +0600

    task, count, btn gray almost done

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ function getTheDate() {
 }
 getTheDate();
 // bg color
+
+
+// blog file
+document.getElementById("blog").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+
 document.getElementById("bg-color").addEventListener("click", function () {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
@@ -15,15 +22,11 @@ document.getElementById("bg-color").addEventListener("click", function () {
   document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
 
-// blog file
-document.getElementById("blog").addEventListener("click", function () {
-  window.location.href = "blog.html";
-});
-
 const taskCounter = document.getElementById("task-counter");
-const navCounter = document.getElementById("nav-counter");
+const headerCounter = document.getElementById("nav-counter");
 const activitySection = document.getElementById("activity-log");
 const historyBtn = document.getElementById("clear-history");
+
 
 const taskMap = {
   1: "task-1",
@@ -33,3 +36,39 @@ const taskMap = {
   5: "task-5",
   6: "task-6",
 };
+
+const cardButtons = document.querySelectorAll(".complete-task");
+let taskCount = parseInt(taskCounter.innerText);
+let navRightCount = parseInt(headerCounter.innerText);
+let btnHittingCount = 0;
+
+// Function to update counters
+function updateCounters() {
+  taskCount--;
+  navRightCount++;
+  btnHittingCount++;
+
+
+  taskCounter.innerText = taskCount;
+  navCounter.innerText = navRightCount;
+}
+for (let i = 0; i < cardButtons.length; i++) {
+  cardButtons[i].addEventListener("click", function () {
+    if (!this.disabled) {
+      alert("Board updated successfully");
+
+      updateCounters();
+      this.disabled = true;
+      this.style.backgroundColor = "gray";
+
+      
+      const assignName = document.getElementById(taskMap[i + 1]).innerText;
+      addActivityLog(assignName);
+
+      if (btnHittingCount === 6) {
+        alert("Congratulations All tasks have finished.");
+      }
+    }
+  });
+}
+
